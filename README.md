f537451b3dd5e737891f1c8c0034445d361913bb
Belladonnixi <jessica_ernst@outlook.de>
2025-01-15 10:59:44 +0100

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# p_16_gallery_app
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 10, 91, 111),
+        ),
+        textTheme: GoogleFonts.urbanistTextTheme(),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
