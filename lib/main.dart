346f6d67ada009ba212d249ec93fd58cfc2b8ee5
Belladonnixi <jessica_ernst@outlook.de>
2025-01-23 22:27:51 +0100

    add comment for portrait Mode

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:p_16_gallery_app/app/app.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  // Startet die App und setzt die App auf Portrait-Modus
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
